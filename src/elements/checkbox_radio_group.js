a0b09f2abe431fa1e008291cbd64ed3ee5e664f2
Peter <github@pixeldepth.net>
2017-06-08 20:54:18 +0100

    Templating for grouped checkboxes and radios

--- src/elements/checkbox_radio_group.js
+++ src/elements/checkbox_radio_group.js
@@ -22,24 +22,44 @@ ProBoards_Post_Forms.Checkbox_Radio_Group = class extends ProBoards_Post_Forms.E
 		for(let f = 0, fl = this.data.length; f < fl; ++ f){
 			html += this.data[f].name + ": ";
 
-			switch(parseInt(this.data[f].type, 10)){
-
-				case 1 :
-					html += "<input type='checkbox' name='" + this.field_for + "-field-grp-" + this.id + "' value='" + this.data[f].value + "' />";
-
-					break;
-
-				case 2 :
-					html += "<input type='radio' name='" + this.field_for + "-field-grp-" + this.id + "' value='" + this.data[f].value + "' />";
-
-					break;
-			}
+			html += this.create_field_html(this.data[f])
 		}
 
 		this.field_html = html;
 	}
 
+	create_field_html(field_data){
+		switch(parseInt(field_data.type, 10)){
+
+			case 1 :
+				return "<input type='checkbox' name='" + this.field_for + "-field-grp-" + this.data[0].group_id + "' value='" + field_data.value + "' />";
+
+				break;
+
+			case 2 :
+				return "<input type='radio' name='" + this.field_for + "-field-grp-" + this.data[0].group_id + "' value='" + field_data + "' />";
+
+				break;
+		}
+	}
+
 	parse(template){
+		let grp_id = this.data[0].group_id;
+
+		template = template.replace("$[" + grp_id + ".group_label]", this.data[0].group_label);
+		template = template.replace("$[" + grp_id + ".group_id]", this.data[0].group_id);
+		template = template.replace("$[" + grp_id + ".field]", this.field_html);
+
+		for(let f = 0, fl = this.data.length; f < fl; ++ f){
+			let pattern = "\\$\\[" + grp_id + "\\.(" + f + "|" + this.data[f].id + ")\\.";
+
+			template = template.replace(new RegExp(pattern + "id]", "g"), this.data[f].id);
+			template = template.replace(new RegExp(pattern + "order]", "g"), this.data[f].order);
+			template = template.replace(new RegExp(pattern + "value]", "g"), this.data[f].value);
+			template = template.replace(new RegExp(pattern + "name]", "g"), this.data[f].name);
+			template = template.replace(new RegExp(pattern + "field]", "g"), this.create_field_html(this.data[f]));
+		}
+
 		return template;
 	}
 
